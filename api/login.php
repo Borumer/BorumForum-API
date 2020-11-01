f9291734bc7bb6b8015327564d316b52b4ec9b78
Varun <31084854+Borumer@users.noreply.github.com>
2020-11-01 16:01:59 -0500

    Implement updateSignIn, LoginTest * Remove RestService.php and Config.php because they were included in Borum-Sleep

--- api/login.php
+++ api/login.php
@@ -2,10 +2,37 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
+use BorumForum\DBHandlers\UserHandler;
+use VarunS\BorumSleep\Helpers;
+use VarunS\BorumSleep\SimpleRest;
+
+$headers = apache_request_headers();
+SimpleRest::handleHeaderValidation($headers, "authorization");
+$userApiKey = Helpers::parseAuthorizationHeader($headers["authorization"]);
+
+$userHandler = new UserHandler($userApiKey);
+
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "POST":
+        $response = $userHandler->createNewUser($_POST["email"], $_POST["password"]);
+        SimpleRest::setHttpHeaders($response["statusCode"]);
+        echo json_encode($response);
     break;
     case "PUT":
+        $_PUT=[];
+        parse_str(file_get_contents('php://input'), $_PUT);
+        $response = $userHandler->updateSignIn($_PUT["new_password"]);
+        SimpleRest::setHttpHeaders($response["statusCode"]);
+        echo json_encode($response);
+    break;
+    default:
+        SimpleRest::setHttpHeaders(405);
+        echo json_encode([
+            "statusCode" => 405,
+            "error" => [
+                "message" => "Invalid request method sent"
+            ]
+        ]);
     break;
 }
 
---
+++ tests/LoginTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class LoginTest extends TestCase {
+    private $http;
+
+    public function setUp() : void {
+        $this->http = new GuzzleHttp\Client(["base_uri" => "https://api.bforborum.com/api/"]);
+    }
+
+    public function testPost() {
+        $response = $this->http->post('login');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json; charset=UTF-8", $contentType);
+    }
+
+    public function testPut() {
+        $response = $this->http->put('login');
+        
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json; charset=UTF-8", $contentType);
+    }
+
+    public function testInvalidRequestMethod() {
+        $response = $this->http->get('GET', 'user-agent');
+
+        $this->assertEquals(405, $response->getStatusCode());
+
+        $contentType = $response->getHeaders()["Content-Type"][0];
+        $this->assertEquals("application/json; charset=UTF-8", $contentType);
+    }
+
+    public function tearDown() : void {
+        $this->http = null;
+    }
+}
+
+?>
