9d6e1852b3e2f405b568d7bc9c2d80189702d6fe
Varun <31084854+Borumer@users.noreply.github.com>
2020-11-01 16:01:59 -0500

    Implement updateSignIn, LoginTest * Remove RestService.php and Config.php because they were included in Borum-Sleep

--- api/include/DBHandlers/UserHandler.php
+++ api/include/DBHandlers/UserHandler.php
@@ -1,9 +1,34 @@
 <?php 
 
 namespace BorumForum\DBHandlers;
+use VarunS\BorumSleep\DBHandlers\UserKnownHandler;
 
-class UserHandler {
+class UserHandler extends UserKnownHandler {
+    function __construct($userApiKey) {
+        parent::__construct($userApiKey);    
+    }
 
+    function createNewUser($email, $password) {
+        return [
+            "statusCode" => 200
+        ];
+    }
+
+    /**
+     * @param string $newPassword The new password
+     */
+    function updateSignIn($newPassword) {
+        $sanitizedNewPassword = mysqli_real_escape_string($this->conn, trim($newPassword));
+        $this->executeQuery("
+        UPDATE users SET password = SHA2('$sanitizedNewPassword', 512) 
+        WHERE id = " . $this->userId . "  
+        LIMIT 1
+        ");
+
+        return [
+            "statusCode" => 200
+        ];
+    }
 }
 
 ?>
