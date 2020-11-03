8607c61f1cd1973e8f4feefa0f956e0db21a549b
Varun <31084854+Borumer@users.noreply.github.com>
2020-11-03 13:35:46 -0500

    Fix bugs * Fix bug in updateSignIn * Fix bug in question.php

--- api/question.php
+++ api/question.php
@@ -5,7 +5,7 @@ require __DIR__ . "/../vendor/autoload.php";
 use BorumForum\DBHandlers\QuestionHandler;
 use VarunS\BorumSleep\SimpleRest;
 
-header('Access-Control-Allow-Methods: POST, PUT, DELETE');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 $headers = apache_request_headers();
 SimpleRest::handleHeaderValidation($headers, "authorization");
