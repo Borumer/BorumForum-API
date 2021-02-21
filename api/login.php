d252d6e9efcc64dbe36c1e747723b54476948cf6
Varun <31084854+Borumer@users.noreply.github.com>
2021-02-21 18:12:11 -0500

    Allow authorization header in login

--- api/login.php
+++ api/login.php
@@ -8,6 +8,7 @@ use VarunS\BorumSleep\SimpleRest;
 
 header('Access-Control-Allow-Methods: POST, PUT, OPTIONS');
 header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+header('Access-Control-Allow-Headers: content-type, authorization');
 
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "POST":
