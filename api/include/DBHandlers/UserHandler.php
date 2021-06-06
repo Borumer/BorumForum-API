36495e19f9e85a4deb3eacc33a3a845eb63fed51
Varun <31084854+Borumer@users.noreply.github.com>
2021-06-06 00:23:35 -0400

    Add GoDaddy config for sendEmail()

--- api/include/DBHandlers/UserHandler.php
+++ api/include/DBHandlers/UserHandler.php
@@ -639,9 +639,12 @@ class UserHandler extends UserNotKnownHandler
         $mailer = new PHPMailer();
 
         //Server settings
-        $mailer->SMTPDebug = 1;                      // Verbose debug output: 1 for no 2,3,4 for yes
+        $mailer->SMTPDebug = 2;                      // Verbose debug output: 1 for no 2,3,4 for yes
         $mailer->isSMTP();                                            // Send using SMTP
         $mailer->Port = 25;                                    // TCP port to connect to
+        $mailer->Host = "localhost";
+        $mailer->SMTPAuth = false;
+        $mailer->SMTPAutoTLS = false;
 
         //Recipients
         $mailer->setFrom('support@borumtech.com', 'Borum Support');
